4e8ab6af083d50705ac1e1187ab651ab7154ceab
光安雅義 <lightcheap358@gmail.com>
2024-03-26 00:13:56 +0900

    mainを少し修正

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,16 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpServer, Responder};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("hello rust and actix web")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("へい　そこ")
-}
-
-async fn manual_hoge() -> impl Responder {
-    HttpResponse::Ok().body("ほげふが")
+async fn index() -> impl Responder {
+    "Hello actix!!"
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-            .route("/hoge", web::get().to(manual_hoge))
+        App::new().service(
+            web::scope("/app")
+                .route("index", web::get().to(index)),
+        )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
