2c45485311cb082f51fb57366f985b1557e8c90d
光安雅義 <lightcheap358@gmail.com>
2024-03-27 00:26:34 +0900

    リロードするたび数が増える処理

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,32 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{web, App, HttpServer};
+use std::sync::Mutex;
+// Shared Mutable state addition
+// リロードするたび数が増える
 
 // この構造体は状態を表します
-struct AppState {
-    app_name: String,
+struct AppStateWithCounter {
+    counter: Mutex<i32>,
 }
 
-#[get("/")]
-async fn index(data: web::Data<AppState>) -> String {
-    let app_name = &data.app_name; // get app_name
-    format!("Hello my name is {app_name}!")   // response
+async fn index(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap();
+    *counter += 1;
+
+    format!("Request number: {counter}")
 }
 
-// アプリの初期化時に状態を渡し、アプリケーションを起動する
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+
+    let counter = web::Data::new(AppStateWithCounter {
+        counter: Mutex::new(0),
+    });
+
+    HttpServer::new(move || {
+
         App::new()
-            .app_data(web::Data::new(AppState {
-                app_name: String::from("actix web!!!"),
-            }))
-            .service(index)
+            .app_data(counter.clone())
+            .route("/", web::get().to(index))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
