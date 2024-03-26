c5038250affbfee27653ecfcfd88ee137343d58b
光安雅義 <lightcheap358@gmail.com>
2024-03-27 00:10:04 +0900

    main再度少し追加修正

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
-use actix_web::{web, App, HttpServer, Responder};
+use actix_web::{get, web, App, HttpServer, Responder};
 
-async fn index() -> impl Responder {
-    "Hello actix!!"
+// この構造体は状態を表します
+struct AppState {
+    app_name: String,
 }
 
+#[get("/")]
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name; // get app_name
+    format!("Hello my name is {app_name}!")   // response
+}
+
+// アプリの初期化時に状態を渡し、アプリケーションを起動する
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(
-            web::scope("/app")
-                .route("index", web::get().to(index)),
-        )
+        App::new()
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("actix web!!!"),
+            }))
+            .service(index)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
