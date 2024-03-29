3309790c874326b266734cc8b4827b7c4eccda96
光安雅義 <lightcheap358@gmail.com>
2024-03-30 08:52:44 +0900

    パス

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{get, web, App, HttpServer, Result};
 
+#[get("/users/{user_id}/{friend}")] // <- define path parameters
+async fn index(path: web::Path<(u32, String)>) -> Result<String> {
+    let (user_id, friend) = path.into_inner();
+    Ok(format!("Welcome {}, user_id {}!", friend, user_id))
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/", web::get().to(HttpResponse::Ok)))
+    HttpServer::new(|| App::new().service(index))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
