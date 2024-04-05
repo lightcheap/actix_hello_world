665aee5b723a00ca975250d37ff2c9ca998f22fb
光安雅義 <lightcheap358@gmail.com>
2024-04-05 22:57:30 +0900

    header,footer追加

--- src/handler.rs
+++ src/handler.rs
@@ -1,8 +1,15 @@
 use log::info;
-use actix_web::{Responder, HttpResponse, get};
+use actix_web::{Responder, HttpResponse, web, get, post};
+
+// mod data;
 
 #[get("/post")]
 pub async fn post() ->impl Responder {
-    info!("Called post");
-    HttpResponse::Ok().body("called post")
+    let mut body_str: String = "".to_string();
+    body_str += include_str!("../static/header.html");
+    body_str += include_str!("../static/footer.html");
+
+    HttpResponse::Ok().content_type("text/html; charset=utf-8").body(body_str)
+    // info!("Called post");
+    // HttpResponse::Ok().body("called post")
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
 use std::io::Result;
-use actix_web::{get, App, HttpServer, Responder, HttpResponse, middleware::Logger};
+use actix_web::{App, HttpServer,
+    middleware::Logger};
 use env_logger::Env;
 
 mod handler;
 
-#[get("/")]
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello, Actix web!")
-}
-
 #[actix_rt::main]
 async fn main() -> Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
-
     HttpServer::new(|| {
         App::new()
             .service(handler::post)
